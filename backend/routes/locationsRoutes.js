be99141bcd89ec3f9641b5059b62f9227bf58426
artssoria <53129227+artssoria@users.noreply.github.com>
2024-07-25 16:44:36 -0300

    restored

--- backend/routes/locationsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const locationsController = require('../controllers/locationsController');
-
-router.get('/provinces', locationsController.getProvinces);
-router.get('/departments/:province', locationsController.getDepartments);
-router.get('/cities/:department', locationsController.getCities);
-
-module.exports = router;
--- backend/routes/professionsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const professionsController = require('../controllers/professionsController');
-
-router.get('/', professionsController.getProfessions);
-
-module.exports = router;
