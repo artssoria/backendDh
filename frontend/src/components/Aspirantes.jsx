f130d2e7928d43a2aa90515c175eec961a3b448c
Eze <choqooz@gmail.com>
2024-07-28 20:20:48 -0300

    fix img

--- frontend/src/components/Aspirantes.jsx
+++ frontend/src/components/Aspirantes.jsx
@@ -15,7 +15,7 @@ const Aspirantes = () => {
             key={person.id_applicants}
             className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
             <div className="box-avatar">
-              <img src={`src/assets/img/${person.image}`} alt={person.name} />
+              <img src={`${person.image}`} alt={person.name} />
             </div>
             <div className="box-bio">
               <h2 className="bio-name">{`${person.first_name} ${person.last_name}`}</h2>
